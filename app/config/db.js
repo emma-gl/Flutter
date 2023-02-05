a16fa2f591a5c35dba32bef7d5eee8a621333386
rgibbons131 <richiegibbons99@gmail.com>
2023-02-05 15:33:18 -0700

    Moved Server to Separate Repo

--- app/config/db.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASS,
-    port: 5432,
-    ssl: true
-   };
-   
