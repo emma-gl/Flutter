09b1f52666163ea61c68086d76519048cb5916dc
isaacpradford <radf.isaac@gmail.com>
2023-03-08 08:33:35 -0700

    Added MVC details

--- webpage/scripts/models/feed_model.js
+++ webpage/scripts/models/feed_model.js
@@ -1,1 +1,6 @@
-// Model is where we interact with the database
+// Gets all the data regarding another profile for it to be built by the Utilities
+const pool = require("../")
+
+async function getUserProfile(){
+    return await pool.query("SELECT * FROM public.users")
+  }
--- webpage/scripts/routes/routes.js
+++ webpage/scripts/routes/routes.js
@@ -1,6 +1,25 @@
 // Gets the request, calls functions from the controller function
+const express = require("express")
+const router = new express.Router()
 const util = require("../utilities/utilities")
 // const feedController = require("../controllers/feed_controller")
 const accController = require("../controllers/account_controller")
+const feedController = require("../controllers/feed_controller")
+
+// Links to access the Backend API
+let urlMain = "https://flutterservices.onrender.com";
+let urlSignIn = "https://flutterservices.onrender.com/api/auth/signin";
+let urlSignUp = "https://flutterservices.onrender.com/api/auth/signup";
+
+
+
+// route to build the profile view
+router.get("/profile", feedController.buildProfile)
+
+
+// Route for account information
+// router.get("/account", accController)
+
+
 
 
