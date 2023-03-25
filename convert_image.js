5d2eeb123b81b274a3d9487e3ccf87b65b6cea8b
Emma Lund <91928663+emma-gl@users.noreply.github.com>
2023-03-25 17:28:50 -0600

    base64 to image

--- convert_image.js
+++ convert_image.js
@@ -1,11 +1,44 @@
-const fileInput = document.getElementById('file-input');
+function toBase64(file){
+  //const fileInput = document.getElementById('file-input');
 
-fileInput.addEventListener('change', async () => {
-  const file = fileInput.files[0];
-  try {
-    const base64String = await convertImageToBase64(file);
-    console.log(base64String);
-  } catch (error) {
-    console.error(error);
-  }
-});
+  fileInput.addEventListener('change', async () => {
+    const file = fileInput.files[0];
+    try {
+      const base64String = await convertImageToBase64(file);
+      console.log(base64String);
+      return base64String;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+async function fileToBase64(file) {
+  //alt function (using promises) https://stackoverflow.com/questions/6150289/how-can-i-convert-an-image-into-base64-string-using-javascript
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+
+    reader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+
+function base64ToImage(base64String) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => {
+      resolve(img);
+    };
+    img.onerror = (error) => {
+      reject(error);
+    };
+    img.src = base64String;
+  });
+}
